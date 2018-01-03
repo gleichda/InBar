077f8362373cc4027579d4646f56c21b2e2cef81
David Gleich <dev.david@familie-gleich.de>
2018-01-03 16:02:57 +0100

    Bars mit User verknüpft

---
+++ WebContent/meineBars.jsp
@@ -0,0 +1,57 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link href="./css/style.css" type="text/css" rel="stylesheet">
+<title>Meine Bars</title>
+</head>
+<body>
+	<header>
+		<%@include file="./fragments/header.jsp"%>
+	</header>
+	<nav>
+		<c:choose>
+			<c:when test="${not empty selfUser.vorname}">
+				<jsp:include page="./fragments/navigationEingeloggt.jsp" />
+			</c:when>
+			<c:otherwise>
+				<jsp:include page="./fragments/navigation.jsp" />
+			</c:otherwise>
+		</c:choose>
+	</nav>
+	<article>
+		<h1>Meine Bars</h1>
+		<table id="meineBars">
+			<tr>
+				<th>BarID</th>
+				<th>Name</th>
+				<th>Adresse</th>
+				<th>Beschreibung</th>
+				<th>Lage</th>
+				<th>Musik</th>
+				<th></th>
+			</tr>
+			<c:forEach var="bar" items="${eigeneBars}">
+				<tr>
+					<td>${bar.barid}</td>
+					<td>"${bar.barname}"</td>
+					<td>"${bar.strasse}" "${bar.hausnummer}" <br>
+						"${bar.plz}" "${bar.ort}"
+					</td>
+					<td>"${bar.bbeschreibung}"</td>
+					<td>"${bar.lbeschreibung}"</td>
+					<td>"${bar.mbeschreibung}"</td>
+				</tr>
+			</c:forEach>
+		</table>
+	</article>
+	
+	<footer>
+		<%@include file="./fragments/fusszeile.jsp"%>
+	</footer>
+</body>
+
+</html>
---
+++ src/inbar/servlets/EigeneBars.java
@@ -0,0 +1,95 @@
+package inbar.servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import inbar.beans.BarBean;
+import inbar.beans.UserBean;
+
+/**
+ * Servlet implementation class EigeneBars
+ */
+@WebServlet("/MeineBars")
+public class EigeneBars extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Resource(lookup = "jdbc/InBar")
+	private DataSource ds;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EigeneBars() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		final HttpSession session = request.getSession();
+		UserBean selfUser = (UserBean) session.getAttribute("selfUser");
+		int userId = selfUser.getUserid();
+		List<BarBean> barsList = new ArrayList<BarBean>();
+		try (Connection con = ds.getConnection();
+				PreparedStatement statement = con.prepareStatement(
+						"SELECT * FROM ((bar LEFT JOIN musik_zu_bar ON bar.barid=musik_zu_bar.barid) LEFT JOIN musikarten ON musik_zu_bar.musikid=musikarten.musikid) LEFT JOIN bar_zu_user ON bar_zu_user.barid=bar.barid WHERE bar_zu_user.userid = ?")) 
+		{
+			statement.setInt(1, userId);
+			ResultSet rs = statement.executeQuery();
+			while (rs.next()) {
+				BarBean bar = new BarBean();
+				bar.setBarid(rs.getInt("bar.barid"));
+				bar.setBarname(rs.getString("bar.barname"));
+				bar.setVorname(rs.getString("bar.vorname"));
+				bar.setNachname(rs.getString("bar.nachname"));
+				bar.setChefmail(rs.getString("bar.chefmail"));
+				bar.setStrasse(rs.getString("bar.strasse"));
+				bar.setHausnummer(rs.getString("bar.hausnummer"));
+				bar.setPlz(rs.getString("bar.plz"));
+				bar.setOrt(rs.getString("bar.ort"));
+				bar.setBarmail(rs.getString("bar.barmail"));
+				bar.setBbeschreibung(rs.getString("bar.bbeschreibung"));
+				bar.setMbeschreibung(rs.getString("bar.mbeschreibung"));
+				bar.setLbeschreibung(rs.getString("bar.lbeschreibung"));
+				barsList.add(bar);
+			}
+			if (!barsList.isEmpty()) {
+				System.out.println("Ergebnisse enthalten");
+				request.setAttribute("eigeneBars", barsList);
+
+				RequestDispatcher dispatcher = request.getRequestDispatcher("meineBars.jsp");
+				dispatcher.forward(request, response);
+			} else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher("keineEigenenBars.jsp");
+				dispatcher.forward(request, response);
+			}
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace(System.out);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
